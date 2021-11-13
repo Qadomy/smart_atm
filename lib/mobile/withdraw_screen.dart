67e3cfbe4f1dfb4d0e485f26fdc4379eea5922c5
Ali Qadomy <ali.qadomy@hotmail.com>
2021-11-13 10:55:15 +0200

    asd

--- lib/mobile/withdraw_screen.dart
+++ lib/mobile/withdraw_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:smart_atm/mobile/scan_qr_screen.dart';
 
 class WithdrawScreen extends StatefulWidget {
   const WithdrawScreen({Key? key}) : super(key: key);
